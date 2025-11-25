a0db983618a5053eff9ac872e88ff195c21ae488
Deepesh Nimma <deepnimma@gmail.com>
2025-11-24 21:38:18 -0500

    pruner

--- main.py
+++ main.py
@@ -13,7 +13,7 @@ import card
 sdk = TCGdex("en")
 
 # CHANGE THIS
-set_id = "wp"
+set_id = "gym2"
 
 # Get set_data
 set_data = requests.get(f"https://api.tcgdex.net/v2/en/sets/{set_id}")
---
+++ prune.py
@@ -0,0 +1,50 @@
+import os
+import json
+
+# MUST BE MOVED TO THE SAME FOLDER AS METADATA AND IMAGES
+# ...
+# | - ./images
+# | - ./metadata
+# | - prune.py
+
+metadata_files = os.listdir("./metadata")
+print(len(metadata_files))
+
+data: list[dict] = []
+for file in metadata_files:
+    with open(f"./metadata/{file}", "r") as file:
+        data.append(json.load(file))
+
+def handle_lt_surge(data: dict) -> dict:
+    lt_owner = "lt-surge"
+    lt_trainer_name = "lt. surge"
+    lt_owner_len = len(lt_owner) + 2
+    lt_cameo_pokemon = ["lt-surge", "lt. surge", "lt surge", "lt.-surge"]
+
+    data["mainPokemon"] = d["mainPokemon"][lt_owner_len::]
+    data["trainerInfo"]["trainerOwned"] = True
+    data["trainerInfo"]["trainer"] = lt_trainer_name
+    data["cameoPokemon"] = lt_cameo_pokemon
+
+    return data
+
+new_data: list[dict] = []
+for d in data:
+    if d["trainerInfo"]["item"]:
+        new_data.append(d)
+    elif d.get("mainPokemon").startswith("lt-surges-"):
+        new_data.append(handle_lt_surge(d))
+    else:
+        owner = d.get("cardTitle").split(" ")[0]
+        # remove 's
+        owner = owner[:-2]
+        owner_len = len(owner) + 2
+        d["mainPokemon"] = d["mainPokemon"][owner_len::]
+        d["trainerInfo"]["trainerOwned"] = True
+        d["trainerInfo"]["trainer"] = owner.lower()
+        d["cameoPokemon"] = [owner.lower()]
+        new_data.append(d)
+
+for i, file_name in enumerate(metadata_files):
+    with open(f"./metadata/{file_name}", "w") as file:
+        json.dump(new_data[i], file, indent=2)
